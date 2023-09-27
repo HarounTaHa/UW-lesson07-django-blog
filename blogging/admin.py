d9e3f0fd93eee5f7966a26336d13eb5b6d6fa3c2
HarounTaHa <harountaha@outlook.sa>
2023-09-27 13:19:35 +0300

    FIXING THE BLOG

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import Post, Category
 
+
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ("posts",)
+
+
+class CategoryInLine(admin.TabularInline):
+    model = Category.posts.through
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInLine,
+    ]
