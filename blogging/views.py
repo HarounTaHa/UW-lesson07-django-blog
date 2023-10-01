0f5a1e45eaa6b774daada31c376f18b74bf20479
HarounTaHa <harountaha@outlook.sa>
2023-10-01 14:48:55 +0300

    Assignment 8 django addons for your blog implement class based view

--- blogging/views.py
+++ blogging/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 from blogging.models import Post
 from django.template import loader
 
 
 # Create your views here.
-
 
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
@@ -18,18 +20,12 @@ def stub_view(request, *args, **kwargs):
     return HttpResponse(body, content_type="text/plain")
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
+class BlogListView(ListView):
+    template_name = 'blogging/list.html'
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
 
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
