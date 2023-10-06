82fc46ea044f9a44813160b0dbb7f466a694ed83
HarounTaHa <harountaha@outlook.sa>
2023-10-06 19:46:50 +0300

    Applied black.

--- polling/views.py
+++ polling/views.py
@@ -6,19 +6,19 @@ from polling.models import Poll
 
 class PollListView(ListView):
     model = Poll
-    template_name = 'polling/list.html'
+    template_name = "polling/list.html"
 
 
 class PollDetailView(DetailView):
     model = Poll
-    template_name = 'polling/detail.html'
+    template_name = "polling/detail.html"
 
     def post(self, request, *args, **kwargs):
         poll = self.get_object()
-        if request.POST.get('vote') == 'Yes':
+        if request.POST.get("vote") == "Yes":
             poll.score += 1
         else:
             poll.score -= 1
         poll.save()
-        context = {'poll': poll}
-        return render(request, 'polling/detail.html', context)
+        context = {"poll": poll}
+        return render(request, "polling/detail.html", context)
