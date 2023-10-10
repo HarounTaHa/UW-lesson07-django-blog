74119513bd59f38cbdfddee0b4e03d63eaa6bd43
HarounTaHa <harountaha@outlook.sa>
2023-10-10 14:36:46 +0300

    Addon #3: Django Rest Framework

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from blogging.views import BlogListView, BlogDetailView, PostViewSet,UserViewSet
+from blogging.views import BlogListView, BlogDetailView, PostViewSet, UserViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
